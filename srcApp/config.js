39616c0575357233cfae2c6faca068861eb41572
natebowang <120029790@qq.com>
2019-06-19 17:15:19 +0800

    MockList add style

--- srcApp/config.js
+++ srcApp/config.js
@@ -1,1 +1,1 @@
-
+const PAGE_SIZE = 10;
--- srcApp/view/MockList.js
+++ srcApp/view/MockList.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
+const style = {
+    margin: '0.4rem',
+    borderRadius: '0.2rem',
+    background: '#e0f0f0',
+    width: '35rem',
+};
+
 const MockList = ({items}) => {
     return (
         <>
             {items.map((item, key)=>{
-                return <div key={key}>{item.id} {item.title}</div>
+                return <div key={key} style={style}>{item.id} {item.title}</div>
             })}
         </>
     );
