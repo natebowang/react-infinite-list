a565179dd438caf71bea6fb67965b3fc6caddfa0
natebowang <120029790@qq.com>
2019-07-11 11:16:20 +0800

    Change BOTTOM_DISTANCE

--- srcApp/config.js
+++ srcApp/config.js
@@ -4,4 +4,4 @@ export const PAGE_SIZE = 10;
 // If this value is 0, ifReachBottomReducer will download new items when reach bottom.
 // If this value is 100, ifReachBottomReducer will download new items when reach
 // 100px above bottom.
-export const BOTTOM_DISTANCE = 100; // px
+export const BOTTOM_DISTANCE = 200; // px
